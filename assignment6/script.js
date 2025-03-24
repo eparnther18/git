6ba12ebd94055e93b014b7606f35a6b3a0f324a9
eparnther18 <eparnther1@pride.hofstra.edu>
2025-03-24 13:17:53 -0400

    added jscript and fixed button issues

---
+++ assignment6/script.js
@@ -0,0 +1,14 @@
+// Display a message in the browser console
+console.log("CSS + JavaScript is powerful!");
+
+// Show an alert message when the page loads
+window.onload = function () {
+    alert("Welcome to Assignment 6 with JavaScript!");
+};
+
+// Function to toggle between light mode and dark mode
+function changeTheme() {
+    document.body.classList.toggle("dark-mode");
+}
+
+ 
